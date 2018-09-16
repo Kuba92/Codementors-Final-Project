93be5bf62859cbba42c140c460733b4eb6806953
Grzegorz Pogorzelski <g.a.pogorzelski@gmail.com>
2018-09-16 21:24:29 +0200

    modified main view

--- src/main/java/pl/codementors/finalstore/view/RoleView.java
+++ src/main/java/pl/codementors/finalstore/view/RoleView.java
@@ -1,7 +1,11 @@
 package pl.codementors.finalstore.view;
 
+import pl.codementors.finalstore.StoreDAO;
+import pl.codementors.finalstore.model.User;
+
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 
@@ -9,12 +13,21 @@ import java.io.Serializable;
 @Named
 public class RoleView implements Serializable {
 
+    @Inject
+    private StoreDAO storeDAO;
+
     public boolean isAdmin() {
         return FacesContext.getCurrentInstance().getExternalContext().isUserInRole("ADMIN");
     }
 
     public boolean isUser() {
         return FacesContext.getCurrentInstance().getExternalContext().isUserInRole("USER");
+    }
+
+    public boolean isAccepted() {
+        User current = storeDAO.findUserByNickname(FacesContext.getCurrentInstance().getExternalContext().getRemoteUser()).orElse(null);
+        return current.isAccepted();
+
     }
 
 }
