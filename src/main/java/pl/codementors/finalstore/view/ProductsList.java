c9887cc523e8839198dded7eab6e2ad8a824c491
Maciej Muchowski <maciej.muchowski@gmail.com>
2018-09-17 19:07:51 +0200

    Basket view and simple working order making.

--- src/main/java/pl/codementors/finalstore/view/ProductsList.java
+++ src/main/java/pl/codementors/finalstore/view/ProductsList.java
@@ -5,6 +5,7 @@ import pl.codementors.finalstore.model.Product;
 
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
@@ -17,6 +18,9 @@ public class ProductsList implements Serializable {
     @EJB
     private StoreDAO dao;
 
+    @Inject
+    private BasketView basketView;
+
     private List<Product> productList;
 
     public List<Product> getProductList(){
@@ -24,6 +28,15 @@ public class ProductsList implements Serializable {
             productList = dao.findAllProducts();
         }
         return productList;
+    }
+
+    public void addProductToBasket(Product product) {
+        if (!basketView.getProductsInBasket().contains(product)) {
+            basketView.addProductToBasket(product);
+        }
+    }
+    public boolean addedToBasketOrNotAvailable (Product product) {
+        return (basketView.getProductsInBasket().contains(product) || !product.isAvailable());
     }
 
 
